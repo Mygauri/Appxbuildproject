c97f946129be53c62077e6936401b7ed19ec202a
Gauri Rathi <168682321+Mygauri@users.noreply.github.com>
2024-07-05 13:00:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,668 @@
+import React from 'expo-status-bar';
+import { Text ,View , StyleSheet, TextInput ,Button , ScrollView , SearchBar , search}  from 'react-native';
+
+export default function App() {
+    return (
+        <ScrollView style={styles.ScrollView}>
+            
+            <View>
+                <View style={styles.header5}></View>
+            <View style={styles.main2}>
+                    <View style={styles.section7}>
+                    <Text style={styles.cont}>
+                        CNR Number</Text>
+                        </View> 
+                    <View style={styles.section8}>  
+                    <Text style={styles.cont}>
+                        Case Status</Text>
+                        </View>
+                    <View style={styles.section9} >
+                    <Text style={styles.cont}>
+                        Cause List</Text>
+                    </View>  
+                    <View style={styles.section10}>
+                        <View style={styles.liv}> 
+                            <Text style={styles.cev5}>128 </Text>
+                        </View>
+                    <Text style={styles.conti}>
+                        My Cases</Text>
+                        </View>
+
+                </View>
+                <View style={styles.container3}>
+
+                <View style={styles.header11}>
+                    <View style={styles.cev1}>
+                    <Text style={styles.dev}>My Case</Text>
+                    </View>
+                    <View style={styles.cev2}>
+                    </View>
+
+                    <View style={styles.cev3}>
+                      <View style={styles.cev4}>
+                        <Text style={styles.cev5}>2</Text>
+                      </View>
+                      
+                    </View>
+
+                       
+                          
+                    
+
+                <Text style={styles.cont}></Text>
+                </View>
+            </View>
+
+                  
+                <Text style={styles.fle}>Please export Your Cases</Text>
+
+                <Text></Text>
+                <Text></Text>
+                <Text></Text>
+
+
+            <View style={styles.container1}>
+
+            <View style={styles.header1}>
+                <Text style={styles.sep}>Disposed Cases (3)</Text>
+            </View>
+            <View style={styles.header2}>
+                <Text style={styles.sep}>Undated Cases (75)</Text>
+            </View>
+            <View style={styles.header3}>
+                <Text style={styles.cont}>July 2024</Text>
+            </View>
+            <View style={styles.header4}>
+                <Text style={styles.cont}>01-07-2024 (2)</Text>
+            </View>
+            </View>
+            <Text></Text>
+
+         
+         <View >
+        <View style={styles.container}>
+            <View style={styles.header }>
+                <Text style={styles.content}>
+                    Civil Judge</Text>
+                </View>
+
+                <View style={styles.main}>
+                    <View style={styles.section1} > 
+                    <Text style={styles.cont}>
+                         Next/Disposal Date</Text>
+                    </View>
+                    <View style={styles.section2}>
+                        <Text style={styles.cont}>
+                            Case Number</Text>
+                        </View>  
+                    <View style={styles.section3}>
+                        <Text style={styles.cont}>
+                            Party Name</Text>
+                        </View>   
+                </View>
+
+                <View style={styles.main1}>
+                    <View style={styles.section4}>
+                    <View style={styles.InnerSection4}>
+                        <View style={styles.InnerSection}>
+                        <Text style={styles.InnersectionText}>
+                            JUL</Text>
+
+                        </View>
+                        <Text style={styles.rew}>1</Text>
+                        <Text style={styles.re}>2024</Text>
+                        <View style={styles.hej}>
+                            <Text style={styles.led}>Pending</Text>
+                        </View>
+                    </View>
+                    </View>
+                    <View style={styles.section5}>  
+                        <Text style={styles.des}>
+                            Cri.Appeal/
+                            100140/2007
+                        </Text>  
+                        <View style={styles.the}>
+                        <Text style={styles.ed}>Steps</Text>
+                        </View>
+                        
+                        </View>
+                    <View style={styles.section6}>
+                        <Text style={styles.des}>
+                            Ms. Audio Trading Co.
+                            Vs Ms. Audio 
+                            Trading Co.
+                        </Text>
+                        </View>  
+
+                </View>
+
+           
+            
+             
+        </View>
+        </View>
+        <Text></Text>
+        
+        
+        
+        
+        
+        
+        <View style={styles.container}>
+            <View style={styles.header }>
+            <Text style={styles.content}>
+                Civil Judge</Text>
+                </View>
+            
+
+                <View style={styles.main}>
+                    <View style={styles.section1}> 
+                    <Text style={styles.cont}>
+                        Next/ Disposal Date</Text>
+                    </View>
+                    <View style={styles.section2}> 
+                    <Text style={styles.cont}> 
+                        Case Number</Text>
+                    </View>  
+                    <View style={styles.section3}>
+                    <Text style={styles.cont}>
+                        Party Name</Text>
+                    </View>     
+                </View>
+
+                <View style={styles.main1}>
+                    <View style={styles.section4}>
+                    <View style={styles.InnerSection4}>
+                    <View style={styles.InnerSection}>
+                    <Text style={styles.InnersectionText}>
+                    JUL</Text>
+                    </View>
+                    <Text style={styles.rew}>1</Text>
+                    <Text style={styles.re}>2024</Text>
+                    <View style={styles.hej}>
+                    <Text style={styles.led}>Pending</Text>
+                    </View>
+                    </View>
+                    </View>
+                        
+                    <View style={styles.section5}>
+                    <Text style={styles.des}>
+                            Cri.Appeal/
+                            100140/2007
+                        </Text>
+                        <View style={styles.the}>
+                        <Text style={styles.ed}>Steps</Text>
+                        </View>
+                       
+                        </View>
+
+                        
+                    <View style={styles.section6}>
+                    <Text style={styles.des}>
+                            Ms. Audio Trading Co.
+                            Vs Ms. Audio 
+                            Trading Co.
+                        </Text>
+                        </View>  
+
+
+                </View>
+
+            
+             
+
+             </View>
+        
+             <Text></Text>
+
+             <View style={styles.container}>
+            <View style={styles.header }>
+                <Text style={styles.content}>
+                    Civil Judge</Text>
+                </View>
+
+                <View style={styles.main}>
+                    <View style={styles.section1} > 
+                    <Text style={styles.cont}>
+                         Next/Disposal Date</Text>
+                    </View>
+                    <View style={styles.section2}>
+                        <Text style={styles.cont}>
+                            Case Number</Text>
+                        </View>  
+                    <View style={styles.section3}>
+                        <Text style={styles.cont}>
+                            Party Name</Text>
+                        </View>   
+                </View>
+
+                <View style={styles.main1}>
+                    <View style={styles.section4}>
+                    <View style={styles.InnerSection4}>
+                    <View style={styles.InnerSection}>
+                    <Text style={styles.InnersectionText}>
+                    JUL</Text>
+                    </View>
+                    <Text style={styles.rew}>1</Text>
+                    <Text style={styles.re}>2024</Text>
+                    <View style={styles.hej}>
+                    <Text style={styles.led}>Pending</Text>
+                    </View>
+                    </View>
+                    </View>
+                    <View style={styles.section5}>  
+                        <Text style={styles.des}>
+                            Cri.Appeal/
+                            100140/2007
+                        </Text>  
+                        <View style={styles.the}>
+                        <Text style={styles.ed}>Steps</Text>
+                        </View>
+                        
+                        </View>
+                    <View style={styles.section6}>
+                        <Text style={styles.des}>
+                            Ms. Audio Trading Co.
+                            Vs Ms. Audio 
+                            Trading Co.
+                        </Text>
+                        </View>  
+
+                </View>
+
+             
+            
+             
+        </View>
+        
+        </View>
+        <Text></Text>
+       
+        <View style={styles.container}>
+            
+            <View style={styles.header }>
+                <Text style={styles.content}>
+                    Civil Judge</Text>
+                </View>
+
+                <View style={styles.main}>
+                    <View style={styles.section1} > 
+                    <Text style={styles.cont}>
+                         Next/Disposal Date</Text>
+                    </View>
+                    <View style={styles.section2}>
+                        <Text style={styles.cont}>
+                            Case Number</Text>
+                        </View>  
+                    <View style={styles.section3}>
+                        <Text style={styles.cont}>
+                            Party Name</Text>
+                        </View>   
+                </View>
+
+                <View style={styles.main1}>
+                    <View style={styles.section4}>
+                        <View style={styles.InnerSection4}>
+                        <View style={styles.InnerSection}>
+                        <Text style={styles.InnersectionText}>
+                        JUL</Text>
+                        </View>
+                        <Text style={styles.rew}>1</Text>
+                        <Text style={styles.re}>2024</Text>
+                        <View style={styles.hej}>
+                        <Text style={styles.led}>Pending</Text>
+                        </View>
+                        </View>
+
+                        </View>
+                    <View style={styles.section5}>  
+                        <Text style={styles.des}>
+                            Cri.Appeal/
+                            100140/2007
+                        </Text>  
+                        <View style={styles.the}>
+                        <Text style={styles.ed}>Steps</Text>
+                        </View>
+                        
+                        
+                        </View>
+                    <View style={styles.section6}>
+                        <Text style={styles.des}>
+                            Ms. Audio Trading Co.
+                            Vs Ms. Audio 
+                            Trading Co.
+                        </Text>
+                        </View>  
+
+                </View>
+
+            
+             
+        </View>
+       
+        <Text></Text>
+
+
+        </ScrollView>
+
+    );   
+}  
+
+const styles = StyleSheet.create({
+    container: {
+        height: 300,
+        gap: 1,
+        backgroundColor: 'grey'
+    },
+    header:{
+        height:50,
+        backgroundColor: 'skyblue',
+        paddingBottom:10
+    },
+    main: {
+        height:60,
+        display: 'flex',
+        flexDirection: 'row',
+        gap:1,
+    },
+    section1: {
+        width: 120,
+        backgroundColor: 'lightgrey',
+        alignItems: 'center'
+        
+    },
+    section2: {
+        width: 150,
+        backgroundColor: 'lightgrey',
+        alignItems:'center'
+    },
+    section3: {
+        width: 130,
+        backgroundColor: 'lightgrey',
+        alignItems: 'center'
+    },
+    main1:{
+        height:200,
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 1,
+    },
+    section4: {
+        width: 120,
+        backgroundColor: 'pink',
+    },
+    section5: {
+        width: 150,
+        backgroundColor: 'pink',
+    },
+    section6: {
+        width: 130,
+        backgroundColor: 'pink',
+    },
+    content: {
+        fontSize: 20,
+        fontWeight: '500',
+        paddingBottom:10
+    },
+    cont: {
+        fontSize: 18,
+        fontWeight: '500',
+        paddingTop:10
+    },
+    header1: {
+        height:50,
+        backgroundColor: 'dimgray',
+    },
+    header2: {
+        height: 50,
+        backgroundColor: 'dimgray',
+    },
+    header3: {
+        height: 50,
+        backgroundColor: 'deepskyblue',
+        alignItems: 'center'
+    },
+    header4: {
+        height: 50,
+        backgroundColor: 'dimgray'
+    },
+    container1: {
+        gap:4,
+        paddingBottom:10
+    },
+    header5: {
+        height: 100,
+        backgroundColor: 'deepskyblue'
+    },
+    main2: {
+        height:50,
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 1,
+    },
+    section7: {
+        width: 100,
+        backgroundColor: 'dimgray'
+    },
+    section8: {
+        width: 100,
+        backgroundColor: 'dimgray',
+        alignItems: 'center'
+
+    },
+    section9: {
+        width: 100,
+        backgroundColor: 'dimgray',
+        alignItems: 'center'
+    },
+    section10: {
+        width: 100,
+        backgroundColor: 'dimgray',
+        alignItems: 'center'
+    },
+    header11: {
+        height: 50,
+        backgroundColor: 'darkgrey',
+        flexDirection: 'row',
+    },
+    des: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        alignItems: 'center',
+        textAlign:'center'
+
+    },
+    sep: {
+        fontSize: 18,
+        fontWeight: '500',
+        color: 'crimson',
+        paddingBottom: 10
+       
+    },
+    ScrollView: {
+        backgroundColor: 'white',
+        margin: 10
+    },
+    InnerSection4: {
+        
+        backgroundColor: 'white',
+        margin:20,
+        width: 80,
+        height: 130,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        borderBottomLeftRadius: 6,
+        borderWidth: 1,
+    },
+    InnerSection: {
+        backgroundColor: 'lightblue',
+        margin: 10,
+        marginTop: 0,
+        marginRight:0,
+        marginLeft: 0,
+        height:30,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+    },
+    InnersectionText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignItems: 'center',
+        textAlign:'center',
+    },
+    rew:  {
+        fontSize: 30,
+        fontWeight: 'bold',
+        alignItems: 'center',
+        textAlign:'center',
+        paddingTop:0,
+        paddingStart: 1,
+    },
+    re: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        alignItems: 'center',
+        textAlign:'center',
+        paddingTop:0,
+        paddingStart: 1,
+    },
+    hej: {
+        backgroundColor: 'green',
+        margin: 10,
+        marginTop: 0,
+        marginRight:0,
+        marginLeft: 5,
+        width:70,
+        height:20,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        borderBottomLeftRadius: 6
+    },
+    led: {
+        textAlign:'center'
+    },
+    the: {
+        backgroundColor: 'white',
+        margin: 10,
+        marginTop: 0,
+        marginRight:0,
+        marginLeft: 5,
+        width:80,
+        height:30,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        borderBottomLeftRadius: 6,
+        textAlign:'center',
+        paddingRight: 10,
+        borderWidth:2,
+        borderColor: 'green'
+    },
+    ed: {
+        textAlign:'center',
+        fontSize:18,
+        fontWeight: 'bold'
+    },
+    cev1: {
+        backgroundColor: 'white',
+        margin: 10,
+        marginTop: 5,
+        marginRight:0,
+        marginLeft: 5,
+        width:80,
+        height:40,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        borderBottomLeftRadius: 6,
+        paddingtop:15
+
+    },
+    dev: {
+        textAlign:'center',
+        fontSize:18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        justifyContent: 'center',
+        textAlignVertical: 'bottom',
+        alignContent: 'center',
+        paddingBottom: 10
+    },
+    cev2: {
+        backgroundColor: 'white',
+        margin: 10,
+        marginTop: 5,
+        marginRight:0,
+        marginLeft: 5,
+        width:50,
+        height:40,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        borderBottomLeftRadius: 6
+    },
+    cev3: {
+        backgroundColor: 'darkgrey',
+        margin: 10,
+        marginTop: 5,
+        marginRight:0,
+        marginLeft: 5,
+        width:50,
+        height:40,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        borderBottomLeftRadius: 6,
+        borderWidth:2,
+        borderColor: 'white'
+    },
+    cev4: {
+        backgroundColor: 'coral',
+        margin: 10,
+        marginTop: 5,
+        marginRight:0,
+        marginLeft: 28,
+        marginBottom: 20,
+        width:15,
+        height:20,
+    },
+    cev5: {
+        alignItems: 'center',
+        textAlign:'center'
+    },
+    liv: {
+        backgroundColor: 'white',
+        margin: 10,
+        marginTop: 2,
+        marginRight:0,
+        marginLeft: 5,
+        width:40,
+        height:15,
+        backgroundColor: 'coral',
+        borderTopRightRadius: 3,
+        borderTopLeftRadius: 3,
+        borderBottomRightRadius: 3,
+        borderBottomLeftRadius: 3,
+    },
+    conti: {
+        fontSize: 18,
+        fontWeight: '500',
+        paddingBottom:15, 
+    },
+    fle: {
+        textAlign:'center',
+        textDecorationLine: 'underline',
+        color: 'red'
+
+    }
+
+    
+    
+
+
+
+    
+})
+            
